6bb315f1bde6b0cbddba3759a117da6356c0f7fa
Pjharris84 <nma21kdu@uea.ac.uk>
2023-01-30 16:26:18 +0000

    added boxplot code

--- scripts/intro_to_stats_wk1.R
+++ scripts/intro_to_stats_wk1.R
@@ -7,4 +7,8 @@ janitor::clean_names(darwin)
 darwin %>% 
   ggplot(aes(x=type,
              y=height))+
-  geom_point()
+  geom_point() #basic scatter plot
+
+ggplot(data = darwin, aes(x = type, y = height)) + 
+  geom_boxplot(aes(fill = type), alpha = 0.7, 
+               width = 0.5, show.legend = FALSE)
