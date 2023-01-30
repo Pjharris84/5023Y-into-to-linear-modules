faa4cf5738bd9398ab0c96a19d52e2c15f446829
Pjharris84 <nma21kdu@uea.ac.uk>
2023-01-30 16:32:13 +0000

    added summary table and figure, kable table created

--- scripts/intro_to_stats_wk1.R
+++ scripts/intro_to_stats_wk1.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(here)
+library(kableExtra)
 
 darwin <- read_csv(here("data", "darwin.csv"))
 janitor::clean_names(darwin)
@@ -12,3 +13,26 @@ darwin %>%
 ggplot(data = darwin, aes(x = type, y = height)) + 
   geom_boxplot(aes(fill = type), alpha = 0.7, 
                width = 0.5, show.legend = FALSE)
+
+darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a new object - summary table of mean and SD for height
+darwin_summary <-darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a summary plot
+darwin_summary %>% 
+  ggplot(aes(x=type,
+             y=mean))+
+  geom_pointrange(aes(ymin=mean-sd, ymax=mean+sd))+
+  theme_bw()
+
+# use kable extra functions to make a nice table (could be replaced with kable() if needed)
+darwin_summary %>% 
+  kbl(caption="Summary statistics of crossed and selfed maize plants") %>% 
+  kable_styling(bootstrap_options = "striped", full_width = T, position = "left")
